323197d0d792ff0210d2df04744cf2fe46540e63
Zia Beg <ziabeg@hotmail.com>
2023-09-11 15:24:49 +0200

    First commit

---
+++ Area.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Area {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int width;
+        int height;
+        int result;
+
+        System.out.println("write 2 numbers");
+
+        width = input.nextInt();
+        height = input.nextInt();
+
+        result = width * height;
+
+        System.out.println("The area is: " + result);
+
+        input.close();
+    }
+}
